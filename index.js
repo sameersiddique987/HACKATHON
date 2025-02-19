951774b9fcbe46a6da53983e00b6b2015b966669
sameersiddique987 <sameersiddique301@gmail.com>
2025-02-19 16:05:24 +0500

    Updated

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ import routes from "./src/routes/user.index.js";
 
 const app = express();
 app.use(express.json());
-
-
+app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:5173",
@@ -17,7 +16,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
+  origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
@@ -25,22 +24,19 @@ app.use(cors({
     }
   },
   credentials: true, 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
-
-
-app.use(express.json());
-app.use(cookieParser());
-
+// ✅ CORS Headers for all responses
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -48,7 +44,6 @@ app.get("/", (req, res) => {
 
 // routes
 app.use("/api/v1", routes);
-
 
 connectDB()
   .then(() => {
